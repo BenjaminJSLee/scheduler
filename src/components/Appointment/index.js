9707a1226c17318a9d5fcc05c8dbf59df87a642a
Benjamin Lee <benjaminjslee@gmail.com>
2020-12-15 15:51:27 -0800

    Add tests in Application.js for more code coverage

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -61,7 +61,7 @@ const Appointment = (props) => {
   }, [props.interview, transition, mode]);
 
   return (
-    <article className="appointment">
+    <article className="appointment" data-testid="appointment">
       <Header time={props.time} />
       { mode === EMPTY && (
         <Empty
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,62 +1,10 @@
 import { useEffect, useReducer } from 'react';
 import axios from "axios";
-
-const SET_DAY = "SET_DAY";
-const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-const SET_INTERVIEW = "SET_INTERVIEW";
-
-const updateSpots = (state, newAppointments, id) => {
-  let newDay = state.days.find((day) => day.appointments.includes(id));
-
-  const spots = newDay.appointments.reduce((numSpots, appId) => {
-    return numSpots + (!newAppointments[appId].interview ? 1 : 0)
-  }, 0);
-
-  newDay = {
-    ...newDay,
-    spots,
-  };
-
-  const days = state.days.map( (day) => {
-    return day.id === newDay.id ? newDay : day;
-  });
-
-  return days;
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case SET_DAY:
-      return {...state, day: action.day};
-    case SET_APPLICATION_DATA:
-      return {...state, 
-        days: action.days, 
-        appointments: action.appointments,
-        interviewers: action.interviewers,
-      }
-    case SET_INTERVIEW: {
-      const appointment = {
-        ...state.appointments[action.id],
-        interview: action.interview && {...action.interview},
-      };
-      const appointments = {
-        ...state.appointments,
-        [action.id]: appointment,
-      };
-
-      const days = updateSpots(state, appointments, action.id);
-
-      return {...state, 
-        days, 
-        appointments,
-      };
-    }
-    default:
-      throw new Error(
-        `Tried to reduce with unsupported action type: ${action.type}`
-      );
-  }
-};
+import reducer, {
+  SET_DAY,
+  SET_APPLICATION_DATA,
+  SET_INTERVIEW,
+} from "reducers/application";
 
 const useApplicationData = () => {
   const [state, dispatch] = useReducer(reducer, {
