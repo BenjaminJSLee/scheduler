77cb860b4d5234fd1967d4e865e411375132033d
Benjamin Lee <benjaminjslee@gmail.com>
2020-12-16 18:49:06 -0800

    Add interviewer validation for form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -13,6 +13,10 @@ const Form = (props) => {
       setError("Student name cannot be blank");
       return;
     }
+    if (interviewer === null) {
+      setError("An interviewer must be selected");
+      return;
+    }
     setError("");
     props.onSave(name, interviewer);
   };
